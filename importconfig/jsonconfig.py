66f710d025699d3552e19ed17a78fc5b0785b1ed
Kasper Jacobsen <k@mackwerk.dk>
2015-01-05 07:50:34 +0100

    Added functions for loading configs directly making the API a bit nicer

--- importconfig/jsonconfig.py
+++ importconfig/jsonconfig.py
@@ -29,3 +29,28 @@ class JsonConfig(ImportConfig):
     def __init__(self, file_path, lazy=False):
         """JsonConfig constructor."""
         super(JsonConfig, self).__init__(json, file_path, lazy=lazy)
+
+
+def jsonconfig(file_path, lazy=False):
+    """Will load a JSON file into a dict.
+
+    Expanding any ``"@file"`` attributes that are found a long the way.
+    It can be used with UltraJSON, simplejson or the built in json module.
+
+    Note:
+        Using this method is the same as calling
+        ``JsonConfig(file_path, lazy=False).load()``
+
+    Arguments:
+        file_path (``str``): The path to the JSON file that will be loaded
+        lazy (``bool``, optional): Do not load the JSON file immediately
+            in the constructor. **default:** ``False``
+
+    Returns:
+        If ``lazy=True`` it will return an instance of ``JsonConfig``
+        else a ``dict`` will be returned.
+    """
+    config = JsonConfig(file_path, lazy=lazy)
+    if lazy:
+        return config
+    return config.load()
